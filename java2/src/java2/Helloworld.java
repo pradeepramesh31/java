973dca5a1f46b08113165e2f2805143e65af6161
sys <sys@sys-PC>
2020-12-17 13:06:15 +0530

    first commmit

---
+++ java2/src/java2/Helloworld.java
@@ -0,0 +1,15 @@
+package java2;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		for(int i=10;i<=99;i++)
+		{
+			if(i%7==0 && i%4==0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
